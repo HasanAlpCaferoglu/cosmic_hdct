6db8526b501efde4d21305f3869042c9b980e8d5
Hasan Alp Caferoglu <alp.caferoglu@gmail.com>
2025-12-17 16:49:03 +0300

    new

---
+++ count_line_in_jsonl.py
@@ -0,0 +1,33 @@
+import os
+import json
+from dotenv import load_dotenv
+
+
+def count_jsonl_lines(file_path: str) -> int:
+    """
+    Count the number of lines in a JSONL file.
+
+    Each line in a JSONL file corresponds to a single JSON object.
+
+    Args:
+        file_path (str): Path to the JSONL file.
+
+    Returns:
+        int: Number of lines in the file.
+    """
+    line_count: int = 0
+    with open(file_path, "r", encoding="utf-8") as file:
+        for _ in file:
+            line_count += 1
+    return line_count
+
+
+if __name__ == "__main__":
+
+    load_dotenv()
+    created_docs_dir = os.getenv("HDCT_DOCS_DIR")
+
+    
+    JSONL_PATH = f"{created_docs_dir}/docs_2000000_4000000.jsonl"  # change this path if needed
+    total_lines: int = count_jsonl_lines(JSONL_PATH)
+    print(f"Number of lines: {total_lines}")
---
+++ run_count_line_in_jsonl.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+# ========================================
+# ===== RESOURCES =====
+# ========================================
+#
+#SBATCH --job-name=hdct_create_docs         #Setting a job name
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=ai                     # avg for lovelace_l40s | ai for ampere_a40
+#SBATCH --account=ai
+#SBATCH --gres=gpu:ampere_a40:1          # lovelace_l40s OR ampere_a40 (use with partition=ai and time:1-0:0:0)
+#SBATCH --output=outputs/count_line_%j.out    # Standard output log file
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=alp.caferoglu@bilkent.edu.tr
+#SBATCH --mem=32G
+
+# ========================================
+# ===== LOADING LIBRARY AND MODULES =====
+# ========================================
+
+module load java/16.0.2
+source activate cosmic
+
+echo "which python:"
+which python
+echo "-------"
+
+echo "which -a python:"
+which -a python
+echo "-------"
+
+echo "gcc version: "
+which gcc
+gcc --version
+echo "-------"
+
+echo "glibc version: "
+ldd --version
+echo "-------"
+
+# Create the log directory if it doesn't exist
+mkdir -p joblogs
+
+# Set stack size to unlimited
+echo "Setting stack size to unlimited..."
+ulimit -s unlimited
+ulimit -l unlimited
+ulimit -a
+
+# Print Cuda
+echo "CUDA Version: $(nvcc --version | grep release | sed 's/.*release //' | sed 's/,.*//')"
+echo "====================================================================================="
+echo "====================================================================================="
+
+export NCCL_P2P_LEVEL=NVL
+export PYTHONUNBUFFERED=TRUE
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+source .env
+
+python3 -u count_line_in_jsonl.py
